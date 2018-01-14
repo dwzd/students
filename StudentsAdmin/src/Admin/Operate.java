bf9ff52b44beba16a233ab9d95ac1bfcf2e346f4
dwzd <dwzd@users.noreply.github.com>
2018-01-14 00:24:59 -0500

    A simple student administration system.

---
+++ StudentsAdmin/src/Admin/Operate.java
@@ -0,0 +1,60 @@
+package Admin;
+
+public class Operate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ManageSystem sys = new ManageSystem();
+		AConsole console = new AConsole();
+		
+		console.println("********Welcome to students administration system!********");
+		
+		while(true) {
+			console.print("Order > ");
+			//Read your input
+			String input = console.readString("");
+			
+			if(input.equals("add")) {
+				console.print("Please input the ID of student:");
+				String id = console.readString("");
+				console.print("Please input the student's fullname:");
+				String name = console.readString("");
+				console.print("Please input your cellphone's number:");
+				String cellphone = console.readString("");
+				
+				Student s = new Student(id, name, cellphone);
+				sys.add(s);  
+				console.println("Success!");
+			}
+			
+			else if (input.equals("list")) {
+				//Print out the content of list
+				sys.list();  
+			}
+			
+			else if(input.equals("find")) {
+				console.print("Please input the name who you are looking for:");
+				String name = console.readString("");
+				Student s = sys.find(name);
+				if (s != null) {
+					console.println("Found the student " + s);
+				}
+				else {
+					console.println("Didn't find!");
+				}
+			}
+			
+			else if(input.equals("remove")) {
+				
+			}
+			
+			else {
+				console.println("Undefined input! " + input);
+			}
+			//Process your input
+			//console.println(input + " Okay!"); 
+			
+		}
+	}
+
+}
---
+++ StudentsAdmin/src/Admin/Student.java
@@ -0,0 +1,51 @@
+package Admin;
+
+//The information of students
+public class Student {
+	private String id;
+	private String name;
+	private String cellPhone;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String id, String name, String cellPhone) {
+		this.id = id;
+		this.name = name;
+		this.cellPhone = cellPhone;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "(" + this.id + ", " + this.name + ", " + this.cellPhone + ")";
+	}
+	
+	
+
+}
