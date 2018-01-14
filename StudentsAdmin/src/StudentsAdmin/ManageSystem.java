a0a3f5aef89dc7ec7a2a7c57d8fb6250f799a909
dwzd <dwzd@users.noreply.github.com>
2018-01-14 00:35:54 -0500

    A simple students administration system.

---
+++ StudentsAdmin/src/StudentsAdmin/ManageSystem.java
@@ -0,0 +1,44 @@
+package StudentsAdmin;
+
+import java.io.Console;
+import java.util.ArrayList;
+
+public class ManageSystem {
+	
+	//To store student objects
+	private ArrayList container = new ArrayList();
+	
+	public ManageSystem() {
+		
+	}
+	
+	//此方法可以添加学生对象
+	public void add (Student s) {
+		container.add(s);
+	}
+	
+	//List all objects of students
+	public void list() {
+		for(int i=0; i<container.size(); i++) {
+		Student s =	(Student)container.get(i);
+		System.out.println("Student:" + s.getName() + " ID: " + s.getId());
+		}
+	}
+	
+	//To find a student by name
+	public Student find(String name) {
+		for(int i=0; i<container.size(); i++) {
+			Student s =	(Student)container.get(i);
+			if (name.equals(s.getName())) {
+				return s;
+			}			
+		}
+		return null;
+	}
+	
+	//To delete a student by id
+	public void remove (String id) {
+		
+	}
+
+}
